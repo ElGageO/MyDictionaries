a0dd4f47866d03567f729ee7eb090f08be160b43
Gage Reynolds <gage_reynolds@baylor.edu>
2022-02-15 21:56:54 -0600

    finished WorldSeriesWinners.py

--- WorldSeriesWinners.py
+++ WorldSeriesWinners.py
@@ -1,27 +1,29 @@
 ws_winners_file = open('WorldSeriesWinners.txt', 'r')
 
-winners_file_list = list(ws_winners_file)
 ws_winners = []
-for line in winners_file_list:
+for line in ws_winners_file:
     team = line.rstrip()
     ws_winners.append(team)
 
 win_counter = {}
+win_years = {}
 
 for team in ws_winners:
     win_count = ws_winners.count(team)
     win_counter[team] = win_count
 
-year_winner = {}
 year = 1903
 
-for num in range(0, len(ws_winners)):
-    year_winner[year] = ws_winners[num]
+for team in ws_winners:
+    win_years[year] = team
     year += 1
+    if year == 1904 or year == 1994:
+        win_years[year] = 'No World Series played'
+        year += 1
 
 user_selection = int(input('\nPlease enter a year: '))
-while user_selection not in range(1903, 2009):
-    user_selection = (int(input('\nThat is not a valid year. Please enter a year: ')))
-
-print('\nThe', user_selection, 'World Series was won by the', year_winner[user_selection])
-print('The', year_winner[user_selection], 'have won the World Series', win_counter[year_winner[user_selection]], 'time(s)\n')
+if user_selection not in range(1903, 2009):
+    user_selection = int(input('\nThat is not a valid year. Please enter a year: '))
+print('\n', user_selection, ' World Series winner: ', win_years[user_selection], sep = '')
+if user_selection != 1904 and user_selection != 1994:
+    print('World Series wins for ', win_years[user_selection], ': ', win_counter[win_years[user_selection]], '\n', sep = '')
